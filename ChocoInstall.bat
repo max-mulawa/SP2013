45b0f0cb8362afc1b93e2ceb2e0dcb8ebcbacaf8
Maksymilian Mulawa <max.mulawa@gmail.com>
2015-05-13 21:18:48 +0200

    Create Sites

---
+++ ChocoInstall.bat
@@ -0,0 +1,25 @@
+choco install notepadplusplus.install --confirm
+choco install sysinternals --confirm
+REM choco install dropbox --confirm
+REM choco install visualstudio2013ultimate
+REM choco install vs2013.powertools
+choco install resharper -version 8.2.3000.5176
+choco install dotpeek --confirm
+choco install linqpad4 --confirm
+choco install git --confirm
+REM choco install githubforwindows --confirm
+choco install tortoisegit --confirm
+choco install fiddler --confirm
+REM choco install virtualbox --confirm
+REM choco install tortoisesvn
+REM choco install winmerge --confirm (failed)
+REM choco install rdcman --confirm
+choco install expresso --confirm
+choco install totalcommander --confirm
+choco install sharepointmanager2013 --confirm
+REM choco install sharepointmanager2010
+choco install ulsviewer --confirm
+REM choco install brackets --confirm (failed)
+choco install google-chrome-x64 --confirm
+choco install powergui --confirm
+
